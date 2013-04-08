74b29d4e3e4fce61e9689e2cdec702b7032a2a68
Thomas Finley <tomfinley@gmail.com>
2013-04-07 23:18:56 -0400

    Removed more extraneous code, added GPL licensing.

--- lib/mobile-menu/init.php
+++ lib/mobile-menu/init.php
@@ -1,25 +1,5 @@
 <?php
-
  
-/** Liquid Slider Vars */
-add_action('genesis_before', 'child_nav_button');
-function child_nav_button() {
- ?>
-      <script>
-		jQuery(document).ready(function(){
-			jQuery(".nav-button").click(function () {
-			jQuery(".nav-button,.mobile-nav").toggleClass("open");
-			});    
-		});
-	</script>
-<?php } 
-
-add_action( 'wp_enqueue_scripts', 'mobile_menu_styles', 200 );
-function mobile_menu_styles() {
-global $wp_styles;
-wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/lib/mobile-menu/style.css', array(), '1.0' );
-}
-
 /** Create Mobile Nav */
 
 add_action('genesis_before_header', 'do_toggle_button', 14);
@@ -44,7 +24,7 @@ function do_menu_wrapper_close() { ?>
 <?php
 }
 
-// End. That's it, folks! //
+// End //
 
 
 
