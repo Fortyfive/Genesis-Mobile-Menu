e57b06c408a92b46d665837139d71d63808900b8
Tom Finley <tom@datachieve.com>
2013-10-07 12:49:28 -0400

    Updated for Genesis 2.0 compatibility

--- functions.php
+++ functions.php
@@ -3,10 +3,5 @@
 /** DO NOT OVERWRITE YOUR CHILD THEME FUNCTIONS.PHP WITH THIS FILE
 	Simply place the snippet below in your existing functions.php, name your nav "Main Nav" and it will call the mobile menu **/
 
-/** Mobile Menu **/
-add_action( 'init', 'genesis_mobile_menu', 9999 );
-
-function genesis_mobile_menu() {
-	if ( ! class_exists( 'mobile_menu' ) )
-		 require_once( CHILD_DIR . '/lib/mobile-menu/init.php' );
-}
+//* Mobile Menu
+require_once(CHILD_DIR.'/lib/mobile-menu/init.php');
--- lib/mobile-menu/init.php
+++ lib/mobile-menu/init.php
@@ -1,30 +1,34 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  
+add_action( 'wp_enqueue_scripts', 'prefix_enqueue_mobile_js' );
+	if ( ! function_exists( 'prefix_enqueue_mobile_js' ) ) {
+		function prefix_enqueue_mobile_js() {
+			wp_enqueue_script( 'prefix-general', get_stylesheet_directory_uri() . '/lib/mobile-menu/js/general.js', array( 'jquery' ), '1.0.0' );
+	}
+}
+
+add_action( 'wp_enqueue_scripts', 'mobile_menu_styles', 200 );
+function mobile_menu_styles() {
+global $wp_styles;
+wp_enqueue_style( 'style', get_stylesheet_directory_uri() . '/lib/mobile-menu/style.css', array(), '1.0' );
+}
+
 /** Create Mobile Nav */
 
-add_action('genesis_before_header', 'do_toggle_button', 14);
-function do_toggle_button() { ?>
-	<div id="mobile-nav-wrapper"><button class="nav-button">Toggle Navigation</button><div class="mobile-label">Navigation</div>
-  <?php
+add_action('genesis_before', 'do_toggle_button', 14);
+function do_toggle_button() {
+	echo '<div id="mobile-nav-wrapper"><a class="menu-toggle" href="#">Toggle Navigation</a><div class="mobile-label">Menu</div>';
+	wp_nav_menu(array(
+	  'menu' => 'Main Nav',
+	  'container' => '',
+	  'menu_class' => 'mobile-nav',
+	  'menu_id' => 'mobile-nav'
+	));
+echo '</div>';
 }
 
-add_action( 'genesis_before_header', 'custom_mobile_nav', 15);
-function custom_mobile_nav() {
-wp_nav_menu(array(
-'menu' => 'Main Nav',
-'container' => '',
-'menu_class' => 'mobile-nav',
-'menu_id' => 'mobile-nav'
-));
-}
-
-add_action('genesis_before_header', 'do_menu_wrapper_close', 16);
-function do_menu_wrapper_close() { ?>
-	</div>
-<?php
-}
-
-// End //
+// End. That's it, folks! //
 
 
 
